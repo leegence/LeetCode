a0964474486b7d1131ae30e47bc354b2c5a7dcd8
leegence <11369830+leegence@users.noreply.github.com>
2018-06-04 22:40:35 -0500

    Update remove-duplicates-from-sorted-array.cpp

--- C++/remove-duplicates-from-sorted-array.cpp
+++ C++/remove-duplicates-from-sorted-array.cpp
@@ -13,3 +13,32 @@ public:
         return last + 1;
     }
 };
+
+
+public int removeDuplicates(int[] nums) {
+    if (nums.length == 0) return 0;
+    int i = 0;
+    for (int j = 1; j < nums.length; j++) {
+        if (nums[j] != nums[i]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+}
+
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+ 
+int main() 
+{
+    std::vector<int> v{1,2,3,1,2,3,3,4,5,4,5,6,7};
+    std::sort(v.begin(), v.end()); 
+    auto last = std::unique(v.begin(), v.end());
+    v.erase(last, v.end());
+    for (const auto& i : v)
+      std::cout << i << " ";
+    std::cout << "\n";
+}
